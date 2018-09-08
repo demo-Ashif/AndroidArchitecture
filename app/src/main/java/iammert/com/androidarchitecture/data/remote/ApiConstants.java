50e99ed3b15538303936b285d6a1bd627e31ff3e
Ashif Mujtoba <nmujtoba@gmail.com>
2018-09-09 00:48:40 +0600

    gradle modified and api key added

--- app/src/main/java/iammert/com/androidarchitecture/data/remote/ApiConstants.java
+++ app/src/main/java/iammert/com/androidarchitecture/data/remote/ApiConstants.java
@@ -8,6 +8,6 @@ public class ApiConstants {
 
     public static final String ENDPOINT = "https://api.themoviedb.org/3/";
     public static final String IMAGE_ENDPOINT_PREFIX = "https://image.tmdb.org/t/p/w500/";
-    public static final String API_KEY = "";
+    public static final String API_KEY = "61093b1979c3a2163b9937fbd9eb345f";
     public static final int TIMEOUT_IN_SEC = 15;
 }
